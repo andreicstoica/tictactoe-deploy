31dde8a64ef5b92e5333a7dd2a3c4ff3220547eb
Andrei Stoica <68513477+andreicstoica@users.noreply.github.com>
2025-06-06 18:31:55 -0400

    fixed frontend link!

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
 export const SERVER_URL = "https://tictactoe-deploy.onrender.com"
 
-export const CLIENT_URL = "https://tac-tac-o9arvsyml-andreicstoicas-projects.vercel.app"
+export const CLIENT_URL = "https://tac-tac-toe.vercel.app"
